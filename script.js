b8eb6bbc3d89f57663a108e90d633ecab769d18d
Sarah <sarahsantiagolenci@gmail.com>
2023-09-12 19:27:49 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let lampada = document.querySelector("#lampada");
+
+function luzAcesa() {
+    lampada.src = "https://mariaccarolina.github.io/L-mpada/img/acesa.jpg";
+}
+
+function luzApagada() {
+    lampada.src = "https://mariaccarolina.github.io/L-mpada/img/lamp.jpg";
+}
